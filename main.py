4e88046f577ee5fe966a562c1939054b749326cb
Marco Nieves <nievesmarcoantonio16@gmail.com>
2024-04-22 21:59:40 -0400

    Adding features

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+"""
+This is a simple Youtube Downloader using pytube and PySide6
+"""
+import sys
+
 from PySide6.QtWidgets import QApplication
 from widget import YoutubeDownloader   
-
-import sys
 
 if __name__ == "__main__":    
     app = QApplication(sys.argv)
--- widget.py
+++ widget.py
@@ -1,14 +1,19 @@
 from PySide6.QtWidgets import QWidget, QLabel, QLineEdit, QPushButton, QHBoxLayout, QVBoxLayout
+from pytube import YouTube
 
 class YoutubeDownloader(QWidget):
-
+    """This is the main widget for the Youtube Downloader"""
 
     def __init__(self):
+        """Initizing"""
+
         super().__init__()
         self.setWindowTitle("PySide6 Youtube Downloader")
         self.init_ui()
 
     def init_ui(self):
+        """This method initialize all the main variables of the will 
+        be used to con build the structure of the widget"""
 
         # Url elements
         self.url_label = QLabel("URL")
@@ -20,6 +25,8 @@ class YoutubeDownloader(QWidget):
 
         # Buttons elements
         self.download_button = QPushButton("Download")
+        self.download_button.clicked.connect(self.download_video(self.url_field.text(), 
+                                                                 self.download_folder_field.text()))
         self.quit_button = QPushButton("Quit")
 
         # Layout elements
@@ -37,13 +44,23 @@ class YoutubeDownloader(QWidget):
 
         self.button_layout.addWidget(self.quit_button)
         self.button_layout.addWidget(self.download_button)
-        
+
         self.v_layout.addLayout(self.url_layout)
         self.v_layout.addLayout(self.folder_layout)
         self.v_layout.addLayout(self.button_layout)
 
         # Setting the main layout
         self.setLayout(self.v_layout)
+
+    def download_video(self, url, filepath):
+        video_url = "https://www.youtube.com/watch?v=UY7ZP0KbBfI"
+        yt = YouTube(video_url)
+        stream_list = yt.streams
+        
+        for stream in stream_list:
+            print(stream)
+
+        
 
 
         
